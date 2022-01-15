a3a452be5f59034d8eeef775a11b065801302b29
Fananek <frantisek.miks@icloud.com>
2022-01-15 20:51:48 +0100

    Initial Commit

---
+++ HexGrid-SwiftUI-Demo/HexGrid_SwiftUI_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HexGrid_SwiftUI_DemoApp.swift
+//  HexGrid-SwiftUI-Demo
+//
+//  Created by Frantisek Miks on 15.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct HexGrid_SwiftUI_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
